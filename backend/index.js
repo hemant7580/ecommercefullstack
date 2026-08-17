89780801bd47d1f33705e126e4c9a7b5569a7362
hemant7580 <hemanttomar7580@gmail.com>
2026-08-17 19:16:47 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["https://ecommercefullstack-frontend-t811.onrender.com","https://ecommercefullstack-admin.onrender.com"],
+  origin: ["https://ecommercefullstack-frontendone.onrender.com","https://ecommercefullstack-admin.onrender.com"],
   credentials: true,
 }))
 
